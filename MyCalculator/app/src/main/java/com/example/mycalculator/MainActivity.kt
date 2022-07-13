2301f3dee5bd0d89486947c6622c81996b5c92c2
demian0721 <83893777+demian0721@users.noreply.github.com>
2022-07-13 20:40:06 +0900

    MyCalculator_2

--- MyCalculator/app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ MyCalculator/app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -1,17 +1,29 @@
 package com.example.mycalculator
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvInput: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvInput = findViewById(R.id.tvInput)
     }
 
-    fun onDigit(view: View){
-        Toast.makeText(this, "ButtonClick", Toast.LENGTH_SHORT).show()
+    fun onDigit(view: View) {
+        tvInput?.append((view as Button).text)
+    }
+
+    fun onClear(view: View) {
+        tvInput?.text = ""
     }
 }
