943404a5c8e18511f5dfb113af2dc26acdcda4a9
demian0721 <83893777+demian0721@users.noreply.github.com>
2022-07-14 17:55:36 +0900

    parkProMainPrac

--- APICallDemo/app/src/main/java/com/example/apicalldemo/MainActivity.kt
+++ APICallDemo/app/src/main/java/com/example/apicalldemo/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.apicalldemo
 
+import android.app.Dialog
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +9,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private inner class CallAPILoginAsyncTask() : AsyncTask<Any, Void, String>(){
+
+        private lateinit var customProgressDialog: Dialog
+
+        override fun onPreExcute(result:String) {
+            super.onPreExecute(result)
+
+            cancelProgressDialog()
+
+        }
+
+        override fun doInBackground(vararg p0: Any?): String {
+            TODO("Not yet implemented")
+        }
+        private fun showProgressDialog() {
+            customProgressDialog = Dialog(this@MainActivity)
+            customProgressDialog.setContentView(R.layout.dialog_custom_progress)
+            customProgressDialog.show()
+        }
     }
 }
