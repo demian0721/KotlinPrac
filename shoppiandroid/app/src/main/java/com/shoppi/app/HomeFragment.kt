cb05d1e4b5c7c19f59a771150137bb9f4eb632c5
demian0721 <83893777+demian0721@users.noreply.github.com>
2022-08-31 11:02:00 +0900

    [섹션7] 강의 예제(홈 화면) JSON asset 추가 및 불러오기 구현

--- shoppiandroid/app/src/main/java/com/shoppi/app/HomeFragment.kt
+++ shoppiandroid/app/src/main/java/com/shoppi/app/HomeFragment.kt
@@ -6,8 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.viewpager2.widget.ViewPager2
+import androidx.viewpager2.widget.ViewPager2.PageTransformer
+import com.bumptech.glide.Glide
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import com.google.android.material.tabs.TabLayoutMediator.TabConfigurationStrategy
+import com.google.gson.Gson
+import org.json.JSONObject
 
 class HomeFragment : Fragment() {
     override fun onCreateView(
@@ -20,14 +30,38 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        val button = view.findViewById<Button>(R.id.btn_enter_product_detail)
-        button.setOnClickListener {
-            findNavController().navigate(R.id.action_home_to_product_detail)
-        }
+        val toolbarTitle = view.findViewById<TextView>(R.id.toolbar_home_title)
+        val toolbarIcon = view.findViewById<ImageView>(R.id.toolbar_home_logo)
+        val viewPager = view.findViewById<ViewPager2>(R.id.viewpager_home_banner)
+        val viewPagerIndicator = view.findViewById<TabLayout>(R.id.viewpager_home_banner_indicator)
 
         val assetLoader = AssetLoader()
-        val homeData = assetLoader.getJsonString(requireContext(), "home.json")
-        Log.d("homeData", homeData ?: "")
+        val homeJsonString = assetLoader.getJsonString(requireContext(), "home.json")
+        Log.d("homeData", homeJsonString ?: "")
+
+        if (!homeJsonString.isNullOrEmpty()) {
+            val gson = Gson()
+            val homeData = gson.fromJson(homeJsonString, HomeData::class.java)
+
+            toolbarTitle.text = homeData.title.text
+            Glide.with(this)
+                .load(homeData.title.iconUrl)
+                .into(toolbarIcon)
+
+            viewPager.adapter = HomeBannerAdapter().apply {
+                submitList(homeData.topBanners)
+            }
+            val pageWidth = resources.getDimension(R.dimen.viewpager_item_width)
+            val pageMargin = resources.getDimension(R.dimen.viewpager_item_margin)
+            val screenWidth = resources.displayMetrics.widthPixels
+            val offset = screenWidth - pageWidth - pageMargin
+            viewPager.setPageTransformer { page, position ->
+                page.translationX = position * -offset
+            }
+            viewPager.offscreenPageLimit = 3
+            TabLayoutMediator(viewPagerIndicator,viewPager) { tab, position ->
+
+            }.attach()
+        }
     }
 }
